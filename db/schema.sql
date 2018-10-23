834f9cc34049b6626d6ee3a6c6dd38c1a0df6fb2
YoungFrank <fluong95@gmail.com>
2018-10-23 14:52:10 -0400

    heroku checkin

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,8 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(30) NOT NULL,
     devoured boolean DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
